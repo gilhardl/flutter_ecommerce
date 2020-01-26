3658b51d9a88a4fc4556275ed20f907f8f8cc4fe
Lucas GILHARD <l.gilhard@gmail.com>
2020-01-26 17:29:39 +0100

    initial commit

---
+++ ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/gilhardl/code/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/gilhardl/code/shrine"
+export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "FLUTTER_FRAMEWORK_DIR=/Users/gilhardl/code/flutter/bin/cache/artifacts/engine/ios"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
