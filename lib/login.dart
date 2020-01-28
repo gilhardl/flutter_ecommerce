b72bfac1cb48c6973e4660ac56f547757e625163
Lucas GILHARD <l.gilhard@gmail.com>
2020-01-28 13:35:40 +0100

    improve login theme

--- lib/login.dart
+++ lib/login.dart
@@ -14,6 +14,8 @@
 
 import 'package:flutter/material.dart';
 
+import 'colors.dart';
+
 class LoginPage extends StatefulWidget {
   @override
   _LoginPageState createState() => _LoginPageState();
@@ -39,21 +41,32 @@ class _LoginPageState extends State<LoginPage> {
               ],
             ),
             SizedBox(height: 120.0),
-            TextField(
-              controller: _usernameTextCtrl,
-              decoration: InputDecoration(filled: true, labelText: 'Username'),
+            AccentColorOverride(
+              color: kShrineBrown900,
+              child: TextField(
+                controller: _usernameTextCtrl,
+                decoration: InputDecoration(
+                  labelText: 'Username',
+                ),
+              ),
             ),
             SizedBox(height: 12.0),
-            TextField(
-              controller: _passwordTextCtrl,
-              decoration: InputDecoration(filled: true, labelText: 'Password'),
-              obscureText: true,
+            AccentColorOverride(
+              color: kShrineBrown900,
+              child: TextField(
+                controller: _passwordTextCtrl,
+                decoration: InputDecoration(
+                  labelText: 'Password',
+                ),
+              ),
             ),
             ButtonBar(
               children: <Widget>[
                 FlatButton(
                   child: Text('Cancel'),
-                  textColor: Theme.of(context).accentColor,
+                  shape: BeveledRectangleBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(7.0)),
+                  ),
                   onPressed: () {
                     _usernameTextCtrl.clear();
                     _passwordTextCtrl.clear();
@@ -61,7 +74,6 @@ class _LoginPageState extends State<LoginPage> {
                 ),
                 RaisedButton(
                   child: Text('Sign in'),
-                  color: Theme.of(context).accentColor,
                   onPressed: () {
                     Navigator.pop(context);
                   },
@@ -75,4 +87,21 @@ class _LoginPageState extends State<LoginPage> {
   }
 }
 
-// TODO: Add AccentColorOverride (103)
+class AccentColorOverride extends StatelessWidget {
+  const AccentColorOverride({Key key, this.color, this.child})
+      : super(key: key);
+
+  final Color color;
+  final Widget child;
+
+  @override
+  Widget build(BuildContext context) {
+    return Theme(
+      child: child,
+      data: Theme.of(context).copyWith(
+        accentColor: color,
+        brightness: Brightness.dark,
+      ),
+    );
+  }
+}
