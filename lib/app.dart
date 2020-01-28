6097582b30a0bb8f627fce6c0d30d9d58c6507ab
Lucas GILHARD <l.gilhard@gmail.com>
2020-01-28 13:12:53 +0100

    app TextTheme

--- lib/app.dart
+++ lib/app.dart
@@ -23,20 +23,18 @@ final ThemeData _kShrineTheme = _buildShrineTheme();
 
 // TODO: Convert ShrineApp to stateful widget (104)
 class ShrineApp extends StatelessWidget {
-  
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Shrine',
-      // TODO: Change home: to a Backdrop with a HomePage frontLayer (104)
-      home: HomePage(),
-      // TODO: Make currentCategory field take _currentCategory (104)
-      // TODO: Pass _currentCategory for frontLayer (104)
-      // TODO: Change backLayer field value to CategoryMenuPage (104)
-      initialRoute: '/login',
-      onGenerateRoute: _getRoute,
-      theme: _buildShrineTheme()
-    );
+        title: 'Shrine',
+        // TODO: Change home: to a Backdrop with a HomePage frontLayer (104)
+        home: HomePage(),
+        // TODO: Make currentCategory field take _currentCategory (104)
+        // TODO: Pass _currentCategory for frontLayer (104)
+        // TODO: Change backLayer field value to CategoryMenuPage (104)
+        initialRoute: '/login',
+        onGenerateRoute: _getRoute,
+        theme: _buildShrineTheme());
   }
 
   Route<dynamic> _getRoute(RouteSettings settings) {
@@ -65,8 +63,29 @@ ThemeData _buildShrineTheme() {
     cardColor: kShrineBackgroundWhite,
     textSelectionColor: kShrinePink100,
     errorColor: kShrineErrorRed,
-    // TODO: Add the text themes (103)
+    textTheme: _buildShrineTextTheme(base.textTheme),
+    primaryTextTheme: _buildShrineTextTheme(base.primaryTextTheme),
+    accentTextTheme: _buildShrineTextTheme(base.accentTextTheme),
     // TODO: Add the icon themes (103)
     // TODO: Decorate the inputs (103)
   );
+}
+
+TextTheme _buildShrineTextTheme(TextTheme base) {
+  return base
+      .copyWith(
+        headline: base.headline.copyWith(
+          fontWeight: FontWeight.w500,
+        ),
+        title: base.title.copyWith(fontSize: 18.0),
+        caption: base.caption.copyWith(
+          fontWeight: FontWeight.w400,
+          fontSize: 14.0,
+        ),
+      )
+      .apply(
+        fontFamily: 'Rubik',
+        displayColor: kShrineBrown900,
+        bodyColor: kShrineBrown900,
+      );
 }
