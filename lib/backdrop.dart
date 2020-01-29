029115a5b5c84180902810ae18205d1c757198b3
Lucas GILHARD <l.gilhard@gmail.com>
2020-01-29 13:27:31 +0100

    add shape to Backdrop to indicate users that ther is a BackLayer

--- lib/backdrop.dart
+++ lib/backdrop.dart
@@ -39,7 +39,9 @@ class _BackdropState extends State<Backdrop>
       children: <Widget>[
         // TODO: Wrap backLayer in an ExcludeSemantics widget (104)
         widget.backLayer,
-        widget.frontLayer,
+        _FrontLayer(
+          child: widget.frontLayer,
+        ),
       ],
     );
   }
@@ -81,6 +83,35 @@ class _BackdropState extends State<Backdrop>
       appBar: appBar,
       // TODO: Return a LayoutBuilder widget (104)
       body: _buildStack(),
+    );
+  }
+}
+
+class _FrontLayer extends StatelessWidget {
+  // TODO: Add on-tap callback (104)
+  const _FrontLayer({
+    Key key,
+    this.child,
+  }) : super(key: key);
+
+  final Widget child;
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      elevation: 16.0,
+      shape: BeveledRectangleBorder(
+        borderRadius: BorderRadius.only(topLeft: Radius.circular(46.0)),
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          // TODO: Add a GestureDetector (104)
+          Expanded(
+            child: child,
+          ),
+        ],
+      ),
     );
   }
 }
